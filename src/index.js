52450d9d3f6686a753fb496100317c93a9de7899
Andrew Yang <andrew@Andrews-MacBook-Pro.local>
2022-04-05 14:46:44 -0400

    add 'read' routes, refactor to use async/await

--- src/index.js
+++ src/index.js
@@ -12,29 +12,96 @@ const port = process.env.PORT || 3000;
 so we can easily access data with request handlers  */
 app.use(express.json());
 
-/* create new user route */
-app.post("/users", (req, res) => {
+/* === CREATE ROUTES === */
+
+/* create new user */
+app.post("/users", async (req, res) => {
     const newUser = new User(req.body);
 
-    newUser.save().then(() => {
+    try {
+        await newUser.save();
         res.status(201).send(newUser);
-    }). catch((e) => {
+    } catch (e) {
         res.status(400).send(e);
-    });
+    }
 });
 
-/* create new shift route */
-app.post("/shifts", (req, res) => {
+/* create new shift */
+app.post("/shifts", async (req, res) => {
     const newShift = new Shift(req.body);
 
-    newShift.save().then(() => {
+    try {
+        await newShift.save();
         res.status(201).send(newShift);
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e);
-    });
+    }
 });
+
+/* === READ ROUTES === */
+
+/* read users */
+app.get("/users", async (req, res) => {
+    
+    try {
+        const users = await User.find({});
+        if (users.length === 0) {
+            return res.status(404).send("no users found");
+        }
+        res.status(200).send(users);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+/* read specific user */
+app.get("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
+            return res.status(404).send("user not found");
+        }
+        res.status(200).send(user);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+/* read shifts */
+app.get("/shifts", async (req, res) => {
+
+    try {
+        const shifts = await Shift.find({});
+        if (shifts.length === 0) {
+            return res.status(404).send("no shifts found");
+        }
+        res.status(200).send(shifts);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+/* read specific shift */
+app.get("/shifts/:id", async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const shift = await Shift.findById(_id);
+        if (!shift) {
+            return res.status(404).send("shift not found");
+        }
+        res.status(200).send(shift);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+
+/* UPDATE ROUTES */
+/* DELETE ROUTES */
 
 app.listen(port, () => {
     console.log("server started on port " + port);
 });
-
